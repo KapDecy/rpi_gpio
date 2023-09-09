6401e4d122e53ffbb70bbaa6d090ce6793dd9f56
z.bykov.d <z.bykov.d@gmail.com>
2023-09-09 15:05:11 +0300

    pupupu

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ use rppal::uart::{Parity, Uart};
 fn main() -> Result<(), Box<dyn Error>> {
     // Connect to the primary UART and configure it for 115.2 kbit/s, no
     // parity bit, 8 data bits and 1 stop bit.
-    let mut uart = Uart::new(115_200, Parity::None, 9, 0)?;
+    let mut uart = Uart::new(115_200, Parity::None, 8, 1)?;
 
     // uart.set_write_mode(true).unwrap();
 
     // let mut i = 0;
 
     loop {
-        uart.write("I'm from rpi".as_bytes()).unwrap();
+        uart.write(&[0xFE, 0, 55, 55, 55, 55, 0, 77, 0xAA]).unwrap();
         // i += 1;
-        std::thread::sleep(Duration::from_secs(1));
+        std::thread::sleep(Duration::from_secs_f32(0.1));
     }
     // Configure read() to block until at least 1 byte is received.
     // uart.set_read_mode(1, Duration::default())?;
